b9f6bacc1e9af1f8e6615dddb603d525c3586a2c
hp997 <hp997@hitesh>
2024-05-16 17:01:45 +0530

    project send

---
+++ day_1_git_push_pull/src/day_1_git_push_pull/controller.java
@@ -0,0 +1,17 @@
+package day_1_git_push_pull;
+
+public class controller {
+
+	public void accountno() {
+		
+		System.out.println("acc no.:-159998760025");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+}
